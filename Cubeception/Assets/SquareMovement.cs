156edffcf6ac4e4a35c9c0e01d306cb9026bf6c6
Yagufrix <antonio.sein@g-tec.io>
2018-11-08 09:35:49 -0600

    SquareMovement
    Moving Square

---
+++ Cubeception/Assets/SquareMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareMovement : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		squareRotation();
+	}
+
+	void squareRotation(){
+		transform.position = transform.position + (new Vector3(speed, 0,0) * Time.deltaTime);
+	}
+}
