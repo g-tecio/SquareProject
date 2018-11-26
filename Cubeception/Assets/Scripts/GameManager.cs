2314f0ba5182a2989e54109792f5fdb41283bd1e
Yagufrix <antonio.sein@g-tec.io>
2018-11-26 09:55:48 -0600

    Animation improve

--- Cubeception/Assets/Scripts/GameManager.cs
+++ Cubeception/Assets/Scripts/GameManager.cs
@@ -30,9 +30,6 @@ public class GameManager : MonoBehaviour {
 
     public void Restart(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        
-        
-
     }
 
 }
