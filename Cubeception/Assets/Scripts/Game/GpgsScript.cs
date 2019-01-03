4fbdb5c2808388e1f51783fcced6d362bd75749d
Yagufrix <antonio.sein@g-tec.io>
2019-01-03 13:32:51 -0600

    Google Services
    Google Services implemented. Login is failing

---
+++ Cubeception/Assets/Scripts/Game/GpgsScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+using TMPro;
+
+public class GpgsScript : MonoBehaviour {
+
+	int points = 0;
+	public TextMeshProUGUI pointsText;
+	// Use this for initialization
+	void Start () {
+		//Debug
+		PlayGamesPlatform.DebugLogEnabled = true;
+
+		//Activate the google play Games platform
+		PlayGamesPlatform.Activate();
+		LogIn();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Login
+	public void LogIn()
+	{
+		Social.localUser.Authenticate((bool sucess) =>
+		{
+			if(sucess)
+			{
+				Debug.Log("Login Sucess");
+			}
+			else
+			{
+				Debug.Log("Login failed");
+			}
+		});
+	}
+
+	public void OnShowLeadBoard()
+	{
+		PlayGamesPlatform.Instance.ShowLeaderboardUI("CgkIz860sswfEAIQAQ");
+	}
+
+	public void addScoreLeaderBoard()
+	{
+		if(Social.localUser.authenticated)
+		{
+			Social.ReportScore(points, "CgkIz860sswfEAIQAQ", (bool sucess) =>
+			{
+				if(sucess)
+				{
+					points = 0;
+					pointsText.text = "Points: " + points;
+				}
+				else
+				{
+					Debug.Log("Update Score Fail");
+				}
+
+			});
+		}
+	}
+
+	public void OnLogOut()
+	{
+		((PlayGamesPlatform)Social.Active).SignOut();
+	}
+}
